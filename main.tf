af4457dd440d805d99bbc0047f9e9e64457d3ca5
Jesse Myers <jesse.myers+git@gmail.com>
2019-03-25 15:24:50 -0700

    Add DKIM

--- main.tf
+++ main.tf
@@ -18,10 +18,23 @@ resource "aws_ses_domain_identity" "jessemyers" {
   domain = "jessemyers.com"
 }
 
-resource "aws_route53_record" "contact_verification_record" {
+resource "aws_route53_record" "domain_identity_verification_record" {
   zone_id = "${data.aws_route53_zone.jessemyers.id}"
-  name    = "_amazonses.example.com"
+  name    = "_amazonses.jessemyers.com"
   type    = "TXT"
   ttl     = "600"
   records = ["${aws_ses_domain_identity.jessemyers.verification_token}"]
+}
+
+resource "aws_ses_domain_dkim" "jessemyers" {
+  domain = "${aws_ses_domain_identity.jessemyers.domain}"
+}
+
+resource "aws_route53_record" "domain_key_verification_record" {
+  count   = 3
+  zone_id = "${data.aws_route53_zone.jessemyers.id}"
+  name    = "${element(aws_ses_domain_dkim.jessemyers.dkim_tokens, count.index)}._domainkey.jessemyers.com"
+  type    = "CNAME"
+  ttl     = "600"
+  records = ["${element(aws_ses_domain_dkim.jessemyers.dkim_tokens, count.index)}.dkim.amazonses.com"]
 }
